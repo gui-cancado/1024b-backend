6e8c40440c16efdde4407c8e75a139852e0df397
gui-cancado <guilherme.cancado@estudante.ifms.edu.br>
2026-03-25 10:18:34 -0400

    ,jhbjnkdzknj.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import mysql from 'mysql2/promise'
 import express from 'express'
 
 const app = express()
+app.use(express.json())
 const connection = mysql.createPool({
     host: 'localhost',
     user: 'root',
@@ -23,6 +24,13 @@ app.post("/pessoas",async(req,res)=>{
     try {
         // const preparecao = await connection.prepare("select * from pessoa")
         const {id,nome} = req.body
+        //Valide se o id e o nome foram passados corretamente. (Algum valor)
+        //Se não foram, retorne o código 400 com a mensagem "id ou nome inválidos"
+        //Não deixe o código executar a parte de baixo quando for inválido.
+
+        if (!id || !nome){
+            res.status(400).json({mensagem:"id ou nome inválidos"})
+        }
         const [resultado, campos] = 
             await connection.execute(`insert into pessoa values (?,?)`, [id, nome])
         console.log(resultado)
